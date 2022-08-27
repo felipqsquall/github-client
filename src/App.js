ac2e91c3d3a9a108bd874a4182a3159f079639fe
Felipe Rosa <felipe.squall@gmail.com>
2022-08-26 22:00:34 -0300

    Concluído

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [username, setUsername] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    if(!username) return;
+    fetch(`https://api.github.com/users/${username}/repos?type=public&sort=updated&per_page=100`)
+    .then(response => response.json())
+    .then((data) => {
+      const result = data.sort((a,b) => (a.stargazers_count < b.stargazers_count ? 1 : -1));
+      
+      setRepos(result);
+    });
+  }, [username]);
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    //https://docs.github.com/pt/rest/search#search-users
+    await fetch(`https://api.github.com/search/users?q=${query}`)
+      .then(response => response.json())
+      .then((data) => setSearchResults(data.items));
+
+    setIsLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="search">
+        <input type="search"
+          value={query}
+          onChange={({ target }) => setQuery(target.value)}
+          placeholder="Username" 
+        />
+        <button onClick={handleSearch} disabled = {isLoading}>{isLoading ? "Pesquisando..." : "Pesquisar"}</button>
+
+        {!!searchResults.length && (
+          <>
+            <h1>Resultado da busca:</h1>
+
+            <ul>
+            {searchResults.map((user) => (
+              <li key={user.id}>
+                <img src={user.avatar_url} alt= {`Foto de ${user.login}`} />
+                {user.login}
+
+                {username === user.login ? (" ✓") : (
+                  <button onClick={() => setUsername(user.login)}>Selecionar</button>
+                )}
+                
+              </li>
+            ))}
+            </ul>
+          </>
+        )}
+
+      </section>
+      <section className="repos">
+        {username ? (
+          <>
+            <h1>Repositórios de {username}:</h1>
+            {repos.length ? (
+                <ul>
+                  {repos.map((repo) => (
+                      <li key={repo.id}>
+                        {repo.name} ({repo.stargazers_count})
+                      </li>)
+                    )
+                  }
+                </ul>
+              ) : ("Carregando...")
+            }
+          </>
+          ) : (<p>Faça uma busca primeiro.</p>)
+        }
+
+      </section>
+
+    </main>
   );
 }
 
